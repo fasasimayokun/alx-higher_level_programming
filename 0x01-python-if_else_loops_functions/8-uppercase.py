1ccef9f6d8a280ade02e845fcae7c0a62ad5853e
fasasimayokun <maykay.fash@gmail.com>
2023-10-03 06:03:02 -0700

    my commit for 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= 97 and ord(c) < 123:
+            ch = chr(ord(char) - 32)
+        print("{}".format(ch), end='')
+    print('')

036fab2db5acf7465cda21a6693e2c8b7f01820f
fasasimayokun <maykay.fash@gmail.com>
2023-10-03 05:32:51 -0700

    my commit for 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for num in range(100):
-    if num >= 0 and num <= 9:
-        print("0{}, ".format(num), end='')
-    elif num > 9 and num <= 98:
-        print("{}, ".format(num), end='')
-    elif num == 99:
+    if num != 99:
+        print("{:02}, ".format(num), end='')
+    else:
         print(num)

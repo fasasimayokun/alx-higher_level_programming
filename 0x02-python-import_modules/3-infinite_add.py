90c93b8c7f232e345c76ecbe02462c4633d44a8d
fasasimayokun <maykay.fash@gmail.com>
2023-10-05 09:17:46 -0700

    my commit for ./3-infinite_add.py 79 10

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    for num in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[num])
+    print("{}".format(sum))

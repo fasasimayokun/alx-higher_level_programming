98f8128a6e1531e7fedac80d6ffaa56e3e7ce372
fasasimayokun <maykay.fash@gmail.com>
2023-10-05 10:14:24 -0700

    my commit for 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,13 @@ if __name__ == "__main__":
     import sys
 
     arg_c = len(sys.argv) - 1
+
     if arg_c == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif arg_c == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(arg_c))
+
     for num in range(arg_c):
         print("{}: {}".format(num + 1, sys.argv[num + 1]))

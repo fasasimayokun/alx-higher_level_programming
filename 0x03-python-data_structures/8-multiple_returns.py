a120e31477a0086af01d1e81f7156cbe7f6e9022
fasasimayokun <maykay.fash@gmail.com>
2023-10-08 02:02:17 -0700

    My commit for 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@
 def multiple_returns(sentence):
     """a func that returns a tuple with the len of a str and its 1st char."""
     if len(sentence) == 0:
-        return (None)
+        return (0, None)
     return (len(sentence), sentence[0])

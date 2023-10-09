f11c7493858e68eafd32b126308b7644c8f69666
fasasimayokun <maykay.fash@gmail.com>
2023-10-09 04:46:11 -0700

    my commit for 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     else:
-        m = 0
+        m = my_list[0]
         for num in my_list:
             if num > m:
                 m = num

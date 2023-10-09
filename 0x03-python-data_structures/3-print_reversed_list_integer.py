307ee28cb0ea10dbda0603202e6120400df1c2f1
fasasimayokun <maykay.fash@gmail.com>
2023-10-09 04:25:47 -0700

    my commit for 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     """a func that prints all ints in a list in reverse order"""
-    my_list = my_list[-1::-1]
-    for num in my_list:
-        print("{:d}".format(num))
+    if isinstance(my_list, list):
+        my_list = my_list[-1::-1]
+        for num in my_list:
+            print("{:d}".format(num))

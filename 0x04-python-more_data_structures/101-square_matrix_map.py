675ad5da4128c3c286877eaa86973874d51265a1
fasasimayokun <maykay.fash@gmail.com>
2023-10-11 06:48:48 -0700

    my commit for 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    """a func that computes the square value of all
-    integers of a matrix using map"""
     new_matrix = list(map(lambda nm: list(map(lambda n: n**2, nm)), matrix))
     return (new_matrix)

9da44af7dd08ba3e6473ce73098e1e729760fece
fasasimayokun <maykay.fash@gmail.com>
2023-10-11 06:53:36 -0700

    my commit for 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """a func that replaces all occurrences of an element by another in a new list."""
+    """a func that replaces all occurrences of
+    an element by another in a new list."""
     new_list = []
     for num in range(len(my_list)):
         if search == my_list[num]:

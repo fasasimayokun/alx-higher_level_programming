7fdfe8766ab40599d080f384e9f3177de4bdd558
fasasimayokun <maykay.fash@gmail.com>
2023-10-11 07:04:20 -0700

    my commit for 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    new_matrix = list(map(lambda nm: list(map(lambda n: n**2, nm)), matrix))
-    return (new_matrix)
+    return (list(map(lambda nm: list(map(lambda n: n**2, nm)), matrix)))

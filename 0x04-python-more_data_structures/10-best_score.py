83833c3d8ac7147a87ec3f500cf9d9df1a59896a
fasasimayokun <maykay.fash@gmail.com>
2023-10-11 06:59:35 -0700

    my commit for 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     """a func that returns a key with the biggest integer value."""
-    if a_dictionary is None:
+    if not a_dictionary:
         return (None)
     m = max(a_dictionary.values())
     for k, val in a_dictionary.items():

3e8396f9be21832bfad53f84e59e3cf89e24be4a
fasasimayokun <maykay.fash@gmail.com>
2023-10-12 01:51:27 -0700

    my commit for roman numeral and 103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_bytes - a func that prints bytes info
+ * @p: the python obj
+ * Return: void(nothing)
+ */
+void print_python_bytes(PyObject *p)
+{
+	char *st;
+	long int len, n, lmt;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf(" [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	len = ((PyVarObject *)(p))->ob_size;
+	st = ((PyBytesObject *)p)->ob_sval;
+
+	printf(" size: %ld\n", len);
+	printf(" trying string: %s\n", st);
+
+	if (len >= 10)
+		lmt = 10;
+	else
+	{
+		lmt = len + 1
+	}
+
+	printf(" first %ld bytes:", lmt);
+	for (n = 0; n < lmt; n++)
+	{
+		if (st[n] >= 0)
+			printf(" %02x", st[n]);
+		else
+			printf(" %02x", 256 + st[n]);
+	}
+	printf("\n")
+}
+
+/**
+ * print_python_list - a func that prints list infor
+ * @p: the python obj
+ * Return: void(nothing)
+ */
+void print_python_list(PyObject *p)
+{
+	long int len, nm;
+	PyListObject *ls;
+	PyObject *inst;
+
+	len = ((PyVarObject *)(p))->ob_size;
+	ls = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", len);
+	printf("[*] Allocated = %ld\n", ls->allocated);
+
+	for (nm = 0; nm < len; nm++)
+	{
+		inst = ((PyListObject *)p)->ob_item[nm];
+		printf("Element %ld: %s\n", nm, ((inst)->ob_type)->tp_name);
+		if (PyBytes_Check(inst))
+		{
+			print_python_bytes(inst);
+		}
+	}
+}
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python3
 
+def rom_sub(ls_num):
+    rm_sb = 0
+    max_nm = max(ls_num)
+
+    for i in ls_num:
+        if max_nm > i:
+            rm_sb = rm_sb + i
+
+    return (max_nm - rm_sb)
+
+
 def roman_to_int(roman_string):
     """a func that converts a Roman numeral to an integer."""
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    roman_nm = {"I": 1, "V": 5, "X": 10,
+                "L": 50, "C": 100, "D": 500, "M": 1000}
+    get_rom_keys = list(roman_nm.keys())
+
+    nm = 0
+    last_roman = 0
+    ls_num = [0]
+
+    for c in roman_string:
+        for rom_key in get_rom_keys:
+            if rom_key == c:
+                if roman_nm[c] <= last_roman:
+                    nm += rom_sub(ls_num)
+                    ls_num = [roman_nm[c]]
+                else:
+                    ls_num.append(roman_nm[c])
+
+                last_roman = roman_nm[c]
+    nm = nm + rom_sub(ls_num)
+    return (nm)

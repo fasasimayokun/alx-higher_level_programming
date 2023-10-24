cd866da1d40fa87832b1c8a89a30827c2bd5e113
fasasimayokun <maykay.fash@gmail.com>
2023-10-24 14:37:12 -0700

    my comit for 3-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,7 +15,7 @@ class Square:
             ValueError: if the size is less than 0
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

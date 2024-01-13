07191a219d5987a7317179355b7dd7acf44f27fb
fasasimayokun <maykay.fash@gmail.com>
2024-01-13 00:25:38 -0800

    my commit for python object relational mapping tasks

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for obj in (session.query(State.name, City.id, City.name)
+                .filter(State.id == City.state_id)):
+        print(obj[0] + ': (' + str(obj[1]) + ') ' + obj[2])

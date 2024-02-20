e52194e705e55c8171f895c919394d09bc8c3c28
fasasimayokun <maykay.fash@gmail.com>
2024-02-20 11:03:18 -0800

    my commit correction for 101 task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,21 +6,21 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+  if (!error) {
     const text = JSON.parse(body);
     const characters = text.characters;
 
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const characterNames = JSON.parse(body);
-          console.log(characterNames.name);
-        }
-      });
-    }
+    displayCharacters(characters, 0);
   }
 });
+
+function displayCharacters (characters, index) {
+  request(characters[index], (error, response, body) => {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        displayCharacters(characters, index + 1);
+      }
+    }
+  });
+}
